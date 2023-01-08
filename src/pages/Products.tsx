3c72add5c39793badc6f6b3a4381163c0aa23cd0
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-01-08 12:03:55 +0100

    added materialUI files

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import CollectionsHero from "../components/CollectionsHero";
 
 function Products() {
-  return <h1>Products Page</h1>
+  return (
+    <React.Fragment>
+      <h1>Products Page</h1>
+      <CollectionsHero />
+    </React.Fragment>
+
+  )
+
 }
 
 export default Products

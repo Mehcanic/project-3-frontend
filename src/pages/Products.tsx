b8e46303c65419b311cd7c82f69a34d2011352fa
Lukasz Bialy <bialy.lukasz0@gmail.com>
2023-01-10 22:16:52 +0000

    configured vite.config.ts and created basic Products.ts

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,14 +2,34 @@ import React from "react";
 import CollectionsHero from "../components/CollectionsHero";
 
 function Products() {
-  return (
+  const [products, setProducts] = React.useState([])
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+  
+  const getProducts = async () => {
+    setLoading(true);
+      const response = await fetch("/api/products");
+      const data = await response.json();
+      setProducts(data);
+  };
 
-      <h1>Products Page</h1>
+  if(!products) {
+    return <h1>Loading...</h1>
+  }
 
+  React.useEffect(() => {
+    getProducts()
+  },[])
 
-
-  )
-
+  return products?.map((product: any) => {
+    return (
+      <>
+        <p>{product.name}</p>
+        <img src={product.images.image_shape1} alt="" />
+      
+      </>
+    )
+  })
 }
 
 export default Products

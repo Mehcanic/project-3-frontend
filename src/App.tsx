0dec42688924adc94bcb4e87a00317788b3d988e
Lukasz Bialy <bialy.lukasz0@gmail.com>
2023-01-12 19:24:13 +0000

    fixed login to properly store userData in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Navbar from "./components/Navbar"
@@ -9,9 +9,22 @@ import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
 import Checkout from "./pages/Checkout"
 import Footer from "./components/Footer"
+import axios from "axios"
+import { IUser } from "./interface/users"
 
 
 function App() {
+  const [user, setUser] = useState<null | IUser>(null)
+
+  async function fetchUser() {
+    const token = localStorage.getItem('token')
+    const { data } = await axios.get('/api/users', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    setUser(data)
+  }
+
+
   return (
     <Router>
       <header>
@@ -23,7 +36,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/custom" element={<Custom />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login fetchUser={fetchUser}/>} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </main>
